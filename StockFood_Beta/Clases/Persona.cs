73bc0af03d168f1dc86109f5f6b9883d0d9e7353
Jafeth Quiros <jafetharrietaquiros@gmail.com>
2022-11-16 15:18:18 -0600

    Agregacion de dos clases
    Creación de las dos clases que se necesitan para entregar el proyecto de programación

---
+++ StockFood_Beta/Clases/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFood_Beta.Clases
+{
+    internal class Persona
+    {
+
+        public int identificacion;
+        public string name;
+        public string puesto;
+        public double salario;
+        public int codigo;
+        public byte nivelDeSeguridad;
+        public string TipoDeApp;
+        public double numeroDeSeguro;
+
+        public void setIdentificacion(int id)
+        {
+            this.identificacion = id;
+        }
+        public int getIdentificacion()
+        { return identificacion; }
+
+        public void setName(string Name)
+        {
+            name = Name;
+        }
+
+    }
+}
---
+++ StockFood_Beta/Clases/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFood_Beta.Clases
+{
+    internal class Producto
+    {
+        public byte Identificacion;
+        public string Name;
+        public string provedor;
+        public int precio;
+        public string tipo;
+        public string casificador;
+
+        
+    }
+}
