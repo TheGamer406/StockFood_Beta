800d4056b4bf705dc054534dd55b02c425464c8b
TheGamer406 <jmurillochevez@gmail.com>
2022-12-08 13:32:24 -0600

    Programacion de la vista de la ventana de inicio de sesión

---
+++ StockFood_Beta/Views/InicioDeSesion.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StockFood_Beta.Views
+{
+    /// <summary>
+    /// Lógica de interacción para InicioDeSesion.xaml
+    /// </summary>
+    public partial class InicioDeSesion : Window
+    {
+        public InicioDeSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void BtnMinimizar_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnIniciarSesion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
